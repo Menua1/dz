82c22e1eb383a7219d4b3d8d12ff20e6e4976cea
Menua <menya2012avah@mail.ru>
2022-11-11 19:25:08 +0300

    1

---
+++ 1=2/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ Задайте прямоугольный двумерный массив.
+ Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+*/
+Console.WriteLine("введите размер массива");
+int massVol = int.Parse(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
+ArrRandNum
+(numbers);
+PrintArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine($"наименьшая сумма  в  {indexLine} строке,  с суммой елементов равной:   {(minsum)}");
+
+void ArrRandNum
+ (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
